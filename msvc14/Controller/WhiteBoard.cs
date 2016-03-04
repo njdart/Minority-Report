762c97fbbb0632ee7b56e2f85d6a655452ea681e
Cameron MacLeod <cameron.macleod@cambridgeconsultants.com>
2016-03-04 11:35:04 +0000

    Change structure of VS project
    Also add VS .gitignore
    There are now 3 projects in solution
     - Controller
     - IAL
     - Display
    This is to allow for building on machines that don't necessarily support
    all parts of the solution

---
+++ msvc14/Controller/WhiteBoard.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minority_Report
+{
+    class WhiteBoard
+    {
+        List<PostIt> postits;
+        List<Arrow> arrows;
+        Bitmap img;
+
+        WhiteBoard()
+        {
+
+        }
+
+        void AddPostIt(Point[] coords, Color color)
+        {
+            
+        }
+    }
+
+    class PostIt
+    {
+        Point[] coords;
+        Color color;
+    }
+
+    class Arrow
+    {
+        Point[] coords;
+    }
+}
