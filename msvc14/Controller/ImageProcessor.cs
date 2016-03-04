8389fa332924df20374f6b93f4965c4958e6f065
Torin Cooper-Bennun <torin.cbennun@googlemail.com>
2016-03-04 16:45:06 +0000

    Building on the KinectHandler framework (formerly called ImageProcessing). Added log4net as logging.

--- msvc14/Controller/ImageProcessor.cs
+++ msvc14/Controller/ImageProcessor.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Emgu.CV;
+using System.Drawing;
 
 namespace Minority_Report
 {
@@ -11,6 +13,11 @@ namespace Minority_Report
         public static PostIt[] getPostIts()
         {
             return new PostIt[3];
+        }
+
+        public static Rectangle findCanvas()
+        {
+            return new Rectangle();
         }
     }
 }
--- msvc14/IAL/Program.cs
+++ msvc14/IAL/Program.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using log4net.Config;
 
-namespace MinorityReport
+namespace Minority_Report.Input_Abstraction
 {
     class MinorityReportApp
     {
         static void Main(string[] args)
         {
-            ImageProcessor imgProc = new ImageProcessor();
+            log.Info("The Input Abstraction Layer's test program is starting up.");
+            KinectHandler kinectHandler = new KinectHandler();
+
+            log.Debug("Attempting to retrieve canvas bounds...");
+            System.Drawing.Rectangle rect = kinectHandler.CanvasBounds;
+            
             for (;;)
             {
                 // Do nothing.
             }
         }
+
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger
+   (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
     }
 }
