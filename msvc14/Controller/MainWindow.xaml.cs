e7f9b739fee1f31d8b7c0808fab01be4974e2c5b
Cameron MacLeod <cameron.macleod@cambridgeconsultants.com>
2016-03-04 16:10:07 +0000

    Things within controller have changed

--- msvc14/Controller/MainWindow.xaml.cs
+++ msvc14/Controller/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Emgu.CV;
 
 namespace Minority_Report
 {
@@ -27,7 +31,26 @@ namespace Minority_Report
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            PostIt[] p = ImageProcessor.getPostIts();
+            List<PostIt> p = ImageProcessor.extractPostIts();
+            Bitmap b = setImage();
+        }
+
+        public Bitmap setImage()
+        {
+            Mat test = CvInvoke.Imread("postits1.jpg", Emgu.CV.CvEnum.LoadImageType.AnyColor);
+            Bitmap img = test.Bitmap;
+            using(MemoryStream memory = new MemoryStream())
+            {
+                img.Save(memory, ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage b = new BitmapImage();
+                b.BeginInit();
+                b.StreamSource = memory;
+                b.CacheOption = BitmapCacheOption.OnLoad;
+                b.EndInit();
+                image.Source = b;
+            }
+            return img;
         }
     }
 }
