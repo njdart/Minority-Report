3d7ee062300d93cacfa0e8ae80af25e99896fcc2
Torin Cooper-Bennun <torin.cbennun@googlemail.com>
2016-03-18 16:21:18 +0000

    moved some stuff around. controller is currently broken

--- src/start.bat
+++ src/start.bat
@@ -3,5 +3,4 @@
 REM Edit the line below in order to provide the correct path.
 SET PATH=%PATH%;C:\Python34
 
-python main.py
-pause
+start pythonw minority_report.py
--- src/startconsole.bat
+++ src/startconsole.bat
@@ -3,4 +3,5 @@
 REM Edit the line below in order to provide the correct path.
 SET PATH=%PATH%;C:\Python34
 
-start pythonw main.py
+python minority_report.py
+pause
