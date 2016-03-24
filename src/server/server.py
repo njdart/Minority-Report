3f2d7832043aebd79273d38ac8aeb3068b7f9533
Cameron MacLeod <cameron.macleod@cambridgeconsultants.com>
2016-03-24 15:14:46 +0000

    Serve Jash's stuff from flask
    Now a route for styles and js
    index.html is now Jash's stub as opposed to Hello World!

--- src/server/server.py
+++ src/server/server.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/js/<path:path>')
+def js_serve(path):
+    return send_from_directory('static/js', path)
+
+@app.route('/styles/<path:path>')
+def css_serve(path):
+    return send_from_directory('static/styles', path)
 
 @socketio.on('my event')
 def test_message(message):
