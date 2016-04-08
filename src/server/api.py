c46e4a648fbc52ff9909453b80d95672aa19bfe6
Nic Dart <nicholasjdart@gmail.com>
2016-04-08 12:17:25 +0000

    Added socket client to index template and added CV2 np array to base64 image encoding

--- src/server/api.py
+++ src/server/api.py
@@ -1,12 +1,27 @@
 from server import (app, socketio)
+from flask_socketio import send, emit
+import numpy as np
+from PIL import Image
+import io
+import base64
+
+import cv2
+
+def npArray2Base64(npArray):
+    img = Image.fromarray(npArray)
+    buffer = io.BytesIO()
+    img.save(buffer, format="JPEG")
+    return base64.b64encode(buffer.getvalue()).decode("utf-8")
+    # $('div').css('background-image', 'url(data:image/gif;base64,' + a.image + ')');
 
 @socketio.on('getPostits')
 def getPostits(request):
+    print("Get Postits" + str(request))
     emit('getPostits', [
         {
             "canvas": "de305d54-75b4-431b-adb2-eb6b9e546014",
             "id": "23a29456-5ded-4b66-b3f0-178b7afdc0e7",
-            "image": "base64-encoded image: see http://stackoverflow.com/questions/26331787/socket-io-node-js-simple-example-to-send-image-files-from-server-to-client",
+            "image": npArray2Base64(cv2.imread('/home/nic/Downloads/postit.jpg')),
             "realX": 10,
             "realY": 10,
             "colour": "red",
@@ -20,6 +35,7 @@ def getPostits(request):
 
 @socketio.on('getCanvas')
 def getCanvas(request):
+    print("Get Canvas" + str(request))
     emit('getCanvas', {
         "id": "de305d54-75b4-431b-adb2-eb6b9e546014",
         "timestamp": "2016-03-18T14:02:56.541Z",
@@ -43,6 +59,6 @@ def getCanvas(request):
                     "23a29456-5ded-4b66-b3f0-178b7afdc0e7"
                 ]
             }
-        }
+        ]
     })
 
