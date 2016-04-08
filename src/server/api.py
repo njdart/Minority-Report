97ee378669e9ff6b70702328ad799b5d0b9c70b1
jashan <jb1352@york.ac.uk>
2016-04-08 16:58:42 +0100

    Improved web UI, added getAll to server

--- src/server/api.py
+++ src/server/api.py
@@ -14,6 +14,49 @@ def npArray2Base64(npArray):
     return base64.b64encode(buffer.getvalue()).decode("utf-8")
     # $('div').css('background-image', 'url(data:image/gif;base64,' + a.image + ')');
 
+@socketio.on('getAll')
+def getAll(request):
+    print("Get all: " + str(request))
+    emit("getAll",
+        {
+            "canvasId": "de305d54-75b4-431b-adb2-eb6b9e546014",
+            "timestamp": "2016-03-18T14:02:56.541Z",
+            "postits": [
+                {
+                  "postitId": "23a29456-5ded-4b66-b3f0-178b7afdc0e7",
+                  "realX": 450,
+                  "realY": 450,
+                  "colour": "red",
+                  "image": npArray2Base64(cv2.imread('/home/jashan/Minority-Report/src/server/static/testPostit1.jpg')),
+                  "connections": [
+                    "36afb67b-c127-4fb8-b795-b917c4099742",
+                    "3fb558b4-5c5c-42a1-98db-84267c470a47"
+                    ]
+                },
+
+                {
+                  "postitId": "36afb67b-c127-4fb8-b795-b917c4099742",
+                  "realX": 790,
+                  "realY": 450,
+                  "colour": "red",
+                  "image": npArray2Base64(cv2.imread('/home/jashan/Minority-Report/src/server/static/testPostit2.jpg')),
+                  "connections": [
+                    "23a29456-5ded-4b66-b3f0-178b7afdc0e7"
+                  ]
+                },
+
+                {
+                  "postitId": "3fb558b4-5c5c-42a1-98db-84267c470a47",
+                  "realX": 1200,
+                  "realY": 970,
+                  "colour": "green",
+                  "image": npArray2Base64(cv2.imread('/home/jashan/Minority-Report/src/server/static/testPostit1.jpg')),
+                  "connections": []
+                }
+            ]
+        }
+    )
+
 @socketio.on('getPostits')
 def getPostits(request):
     print("Get Postits" + str(request))
@@ -21,7 +64,7 @@ def getPostits(request):
         {
             "canvas": "de305d54-75b4-431b-adb2-eb6b9e546014",
             "id": "23a29456-5ded-4b66-b3f0-178b7afdc0e7",
-            "image": npArray2Base64(cv2.imread('/home/nic/Downloads/postit.jpg')),
+            "image": npArray2Base64(cv2.imread('/home/jashan/Minority-Report/src/server/statictestPostit1.jpg')),
             "realX": 10,
             "realY": 10,
             "colour": "red",
