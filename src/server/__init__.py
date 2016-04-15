b1661f3842708b0533e2272d5dc33fe5f6aa085e
Torin Cooper-Bennun <torin.cbennun@googlemail.com>
2016-04-15 10:26:01 +0100

    Moved Kinect client source code into 'src' directory

--- src/server/__init__.py
+++ src/server/__init__.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-
 def databaseHandler():
     if not hasattr(g, "databaseHandler"):
         g.databaseHandler = ModelDatabase
@@ -16,9 +15,7 @@ from src.server import ui
 from src.server import kinect
 from src.server import api
 
-
 def run_server():
-
     socketio.run(app, host="0.0.0.0", port=8088, debug=True)
 
 if __name__ == '__main__':
