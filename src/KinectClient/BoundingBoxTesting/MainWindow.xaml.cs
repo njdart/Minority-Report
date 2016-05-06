a217444ca378dd4dd5e543ca11e5b2ba068d3bd0
Torin Cooper-Bennun <torin.cbennun@googlemail.com>
2016-05-06 16:38:18 +0100

    fixed bounding box detection (added WPF app for debugging)

---
+++ src/KinectClient/BoundingBoxTesting/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MinorityReport;
+
+namespace BoundingBoxTesting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private KinectClient kinectClient = null;
+
+        private DrawingGroup drawingGrp;
+        private DrawingImage drawingImg;
+
+        public ImageSource ImageSource
+        {
+            get
+            {
+                return drawingImg;
+            }
+        }
+
+        public MainWindow()
+        {
+            this.kinectClient = new KinectClient("server", 8088);
+            this.kinectClient.BoundingBoxesSampled += KinectClient_BoundingBoxesSampled;
+            this.kinectClient.BeginSampling();
+
+            InitializeComponent();
+
+            this.drawingGrp = new DrawingGroup();
+            this.drawingImg = new DrawingImage(this.drawingGrp);
+            this.mainCanvas.DataContext = this;
+        }
+
+        private void KinectClient_BoundingBoxesSampled(object sender, BoundingBoxesEventArgs e)
+        {
+            this.Dispatcher.Invoke((Action)(() =>
+            {
+                using (DrawingContext dc = this.drawingGrp.Open())
+                {
+                    dc.DrawRectangle(Brushes.White, null, new Rect(new Size(512.0, 424.0)));
+
+                    foreach (BodyBoundingBox box in e.BoundingBoxes.Where(x => x != null))
+                    {
+                        Rect r = new Rect(box.topLeft, box.bottomRight);
+                        dc.DrawRectangle(Brushes.Blue, null, r);
+                    }
+                }
+            }));
+        }
+    }
+}
