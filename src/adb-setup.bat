e62fbcc26b2fe896a9fd766fd6543c8ea6996c48
Torin Cooper-Bennun <torin.cbennun@googlemail.com>
2016-06-02 11:03:19 +0100

    adb-setup.bat checks for admin permissions; removed canvas_data.zip

--- src/adb-setup.bat
+++ src/adb-setup.bat
@@ -1,6 +1,19 @@
 @echo off
+rem Run as administrator!
+
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    echo Setting up forward to localhost:8080...
+) else (
+    echo You must run this script as administrator!
+    goto end
+)
+
 set PATH=%PATH%;C:\adb
 netsh interface portproxy delete v4tov4 listenport=8080 listenaddress=0.0.0.0
 adb forward tcp:8080 tcp:8080
 netsh interface portproxy add v4tov4 listenport=8080 listenaddress=0.0.0.0 connectport=8080 connectaddress=127.0.0.1
+echo Done.
+
+:end
 pause
